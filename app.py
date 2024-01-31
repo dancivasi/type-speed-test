1a59c47edf8ef0a10f707330046f7c58ecf922c1
dancivasi <vasilicavasilica26@yahoo.com>
2024-01-31 13:15:51 +0200

    finished the app.py but i still have to refactor the test_app part when i get home

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import random
+import time
 
-class Game():
 
-    def choose_language(self, choice):
+class Game:
+
+    @staticmethod
+    def choose_language(choice):
         choice = choice.upper()
         if choice == "ENGLISH":
             return f"{choice} Language Selected."
         else:
             return f"{choice} Language Selected."
 
-    def get_random_sentence(self, choice):
+    @staticmethod
+    def get_random_sentence(choice):
         filepath = choice.upper()
         with open(f"{filepath.lower()}_sentences.txt", "r") as file:
             sentences = file.readlines()
@@ -17,4 +21,24 @@ class Game():
         random_sentence = random_sentence.strip()
         return random_sentence
 
-    
+    @staticmethod
+    def type_speed_test(sentence, user_input ):
+        while user_input != sentence:
+            user_input = input()
+
+
+
+
+if __name__ == "__main__":
+    game = Game()
+    choice = input("Choose language of the type-test:(romanian/english)")
+    sentence = game.get_random_sentence(choice)
+    print("Type the following sentence:")
+    print(sentence)
+    user_input = ""
+    start = time.time()
+    game.type_speed_test(sentence, user_input)
+    end = time.time()
+    elapsed_time = end - start
+    print(f"\nTime elapsed: {elapsed_time} seconds")
+
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,25 @@
 import pytest
 from app import Game
+
+
 def test_user_can_choose_between_english_or_romanian():
     game = Game()
     choice = "english"
     assert game.choose_language(choice) == "ENGLISH Language Selected."
 
-def test_a_sentence_is_shown_in_console():
+def test_an_english_sentence_is_shown_in_console():
     game = Game()
     choice = "english"
-    assert game.get_random_sentence(choice) == "As the sun dipped below the horizon, the city lights flickered to life, transforming the urban landscape into a sea of brilliance."
+    with open("english_sentences.txt", "r") as file:
+        content = file.readlines()
+        content = [phrase.strip() for phrase in content]
+    assert game.get_random_sentence(choice) in content
+
+def test_an_romanian_sentence_is_shown_in_console():
+    game = Game()
+    choice = "romanian"
+    with open("romanian_sentences.txt", "r") as file:
+        content = file.readlines()
+        content = [phrase.strip() for phrase in content]
+    assert game.get_random_sentence(choice) in content
+
